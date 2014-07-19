c4dc830de00b52cf664733cede2bf35613503100
fsuarez6 <fsuarez6@gmail.com>
2014-07-19 17:31:44 -0400

    Added metrics database validation to improve ik teleoperation

--- src/cartesian_controller.cpp
+++ src/cartesian_controller.cpp
@@ -11,14 +11,18 @@
 #include <std_msgs/Float64.h>
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/PoseStamped.h>
-//~ #include "grips_msgs/GripsState.h"
 #include <baxter_core_msgs/JointCommand.h>
 #include <baxter_core_msgs/EndpointState.h>
+
+// Flann
+#include <float.h>
+#include <flann/flann.hpp>
+#include <flann/io/hdf5.h>
 
 // Joint limits
 #include <joint_limits_interface/joint_limits_urdf.h>
 
-// Grips kinematic Interface
+// Moveit Kinematics Interface
 #include <moveit/kinematics_interface/kinematics_interface.h>
 
 using namespace moveit;
@@ -37,6 +41,9 @@ class CartesianController
     KinematicsInterfacePtr                     ik_kinematics_;
     Eigen::Affine3d                           end_effector_pose_;
     std::map<std::string, joint_limits_interface::JointLimits> urdf_limits_;
+    // flann
+    flann::Index<flann::L2<float> >*          position_index_;
+    std::map<std::string, flann::Matrix<float> >  metrics_db_;
     // Time
     ros::Time                                 last_state_print_;
     ros::Time                                 last_motion_print_;
@@ -53,10 +60,24 @@ class CartesianController
       nh_private_("~")
     { 
       // Get parameters from the server
+      nh_private_.param(std::string("position_error"), position_error_, 0.1);
       nh_private_.param(std::string("publish_rate"), publish_rate_, 100.0);
-      if (!nh_private_.hasParam("publish_rate"))
-        ROS_WARN_STREAM("Parameter [~publish_rate] not found, using default: " << publish_rate_ << " Hz");      
+      std::string database_name, folder_key, file_key;
+      nh_private_.param(std::string("metrics_database"), database_name, std::string("ik_metrics"));
+      nh_private_.param(std::string("folder_key"), folder_key, std::string("6575ecddff8e44245a6d08f7e6a232f6"));
+      nh_private_.param(std::string("file_key"), file_key, std::string("dc12ea999aabf684e4dab37176899cd2"));
       
+      if (!nh_private_.hasParam("position_error"))
+        ROS_WARN_STREAM("Parameter [~position_error] not found, using default: " << position_error_ << " m.");
+      if (!nh_private_.hasParam("publish_rate"))
+        ROS_WARN_STREAM("Parameter [~publish_rate] not found, using default: " << publish_rate_ << " Hz");
+      if (!nh_private_.hasParam("metrics_database"))
+        ROS_WARN_STREAM("Parameter [~metrics_database] not found, using default: " << database_name);
+      if (!nh_private_.hasParam("folder_key"))
+        ROS_WARN_STREAM("Parameter [~folder_key] not found, using default: " << folder_key);
+      if (!nh_private_.hasParam("file_key"))
+        ROS_WARN_STREAM("Parameter [~file_key] not found, using default: " << file_key);
+              
       // Get robot namespace
       robot_namespace_ = ros::this_node::getNamespace();
       if (robot_namespace_.rfind("/") != robot_namespace_.length()-1)
@@ -83,10 +104,33 @@ class CartesianController
       motion_control_sub_ = nh_.subscribe(topic_name.c_str(), 1, &CartesianController::ikCommandCB, this); 
       topic_name = "/baxter/pose";
       pose_publisher_ = nh_.advertise<geometry_msgs::PoseStamped>(topic_name.c_str(), 1);
+      
+      // Load the previously generated metrics
+      std::string filename;
+      filename = getFilename(database_name, folder_key, file_key);
+      ROS_INFO_STREAM("Loading [metrics database] from:\n" << filename);
+      ros::Time flann_start_time = ros::Time::now();
+      try {
+        flann::load_from_file(metrics_db_["positions"], filename, "positions");
+        flann::load_from_file(metrics_db_["orientations"], filename, "orientations");
+        flann::load_from_file(metrics_db_["metrics"], filename, "metrics");
+        flann::load_from_file(metrics_db_["joint_states"], filename, "joint_states");
+      }
+      catch (...) {
+        ROS_ERROR_STREAM("Failed loading [metrics database] from:\n" << filename);
+        ros::shutdown();
+        return;
+      }
+      // Load the previously generated index
+      std::ostringstream index_file;
+      index_file << getFolderName(folder_key) << "ik_metrics_index" << file_key << ".dat";
+      flann::SavedIndexParams saved_params = flann::SavedIndexParams(index_file.str());
+      position_index_ = new flann::Index<flann::L2<float> > (metrics_db_["positions"], saved_params);
+      double elapsed_time = (ros::Time::now() - flann_start_time).toSec();
+      ROS_INFO("[metrics database] successfully loaded in %.2f seconds", elapsed_time);
 
       // Setup timer for publishing pose at 60 Hz
       pose_timer_ = nh_.createTimer(ros::Duration(1.0/60), &CartesianController::publishPose, this);
-
       last_motion_print_ = ros::Time::now();
     }
     
@@ -139,80 +183,80 @@ class CartesianController
         ik_kinematics_->getJointPositions(new_joint_values);
       else
       {
-        return;
-        //~ // Check that the IK command has changed enought
-        //~ Eigen::Affine3d goal_pose, current_pose = ik_kinematics_->getEndEffectorTransform();
-        //~ tf::poseMsgToEigen(_msg->pose, goal_pose);
-        //~ double x_dist = goal_pose.translation().x() - current_pose.translation().x();
-        //~ double y_dist = goal_pose.translation().y() - current_pose.translation().y();
-        //~ double z_dist = goal_pose.translation().z() - current_pose.translation().z();
-        //~ double xyz_dist = sqrt(pow(x_dist,2) + pow(y_dist,2) + pow(z_dist,2));
-        //~ 
-        //~ if (xyz_dist < position_error_)
-          //~ return;
-        //~ 
-        //~ ROS_DEBUG("Did not find IK solution");
-        //~ // Determine nn closest XYZ points in the reachability database
-        //~ int max_nn = 1000, nearest_neighbors;
-        //~ flann::Matrix<float> query_pos(new float[3], 1, 3);
-        //~ query_pos[0][0] = _msg->pose.position.x;
-        //~ query_pos[0][1] = _msg->pose.position.y;
-        //~ query_pos[0][2] = _msg->pose.position.z;
-        //~ flann::Matrix<int> indices(new int[query_pos.rows*max_nn], query_pos.rows, max_nn);
-        //~ flann::Matrix<float> dists(new float[query_pos.rows*max_nn], query_pos.rows, max_nn);
-        //~ // do a knn search, using 128 checks
-        //~ // this->index_pos->knnSearch(query_pos, indices, dists, nn, flann::SearchParams(128));
-        //~ nearest_neighbors = indices.cols;
-        //~ float radius = pow(position_error_, 2);
-        //~ nearest_neighbors = position_index_->radiusSearch(query_pos, indices, dists, radius, flann::SearchParams(128));
-        //~ // Check that we found something
-        //~ if (nearest_neighbors <= 0) {
-          //~ ROS_INFO_THROTTLE(60, "Didn't find any shit. Query: [%.3f, %.3f, %.3f]", _msg->pose.position.x, _msg->pose.position.y, _msg->pose.position.z);
-          //~ return; }
-        //~ nearest_neighbors = fmin(nearest_neighbors, max_nn);
-        //~ std::vector<float> score(nearest_neighbors), d_q(nearest_neighbors), 
-                           //~ d_j(nearest_neighbors), d_xyz(nearest_neighbors);
-        //~ Eigen::Quaterniond q_actual(end_effector_pose_.rotation());
-        //~ Eigen::Quaterniond q;        
-        //~ for (std::size_t i=0; i < nearest_neighbors; ++i)
-        //~ {
-          //~ // http://math.stackexchange.com/questions/90081/quaternion-distance
-          //~ q.w() = metrics_db_["orientations"][i][0];
-          //~ q.x() = metrics_db_["orientations"][i][1];
-          //~ q.y() = metrics_db_["orientations"][i][2];
-          //~ q.z() = metrics_db_["orientations"][i][3];
-          //~ d_q[i] = 1 - pow(q.dot(q_actual), 2.0);
-          //~ d_xyz[i] = sqrtf(dists[0][i]);
-          //~ d_j[i] = 0;
-          //~ for(std::size_t j=0; j < joint_names_.size(); ++j)
-          //~ {
-            //~ float j_error = fabs(current_joint_values[i] - metrics_db_["joint_states"][indices[0][i]][j]);
-            //~ d_j[i] = fmax(d_j[i], j_error);
-          //~ }
-          //~ // score[i] = d_q[i] + d_xyz[i] + d_j[i];
+        // Check that the IK command has changed enought
+        Eigen::Affine3d goal_pose, current_pose = ik_kinematics_->getEndEffectorTransform();
+        tf::poseMsgToEigen(_msg->pose, goal_pose);
+        double x_dist = goal_pose.translation().x() - current_pose.translation().x();
+        double y_dist = goal_pose.translation().y() - current_pose.translation().y();
+        double z_dist = goal_pose.translation().z() - current_pose.translation().z();
+        double xyz_dist = sqrt(pow(x_dist,2) + pow(y_dist,2) + pow(z_dist,2));
+        
+        if (xyz_dist < position_error_)
+          return;
+        
+        ROS_DEBUG("Did not find IK solution");
+        // Determine nn closest XYZ points in the reachability database
+        int max_nn = 1000, nearest_neighbors;
+        flann::Matrix<float> query_pos(new float[3], 1, 3);
+        query_pos[0][0] = _msg->pose.position.x;
+        query_pos[0][1] = _msg->pose.position.y;
+        query_pos[0][2] = _msg->pose.position.z;
+        flann::Matrix<int> indices(new int[query_pos.rows*max_nn], query_pos.rows, max_nn);
+        flann::Matrix<float> dists(new float[query_pos.rows*max_nn], query_pos.rows, max_nn);
+        // do a knn search, using 128 checks
+        // this->index_pos->knnSearch(query_pos, indices, dists, nn, flann::SearchParams(128));
+        nearest_neighbors = indices.cols;
+        float radius = pow(position_error_, 2);
+        nearest_neighbors = position_index_->radiusSearch(query_pos, indices, dists, radius, flann::SearchParams(128));
+        // Check that we found something
+        if (nearest_neighbors <= 0) {
+          ROS_INFO_THROTTLE(60, "Didn't find any shit. Query: [%.3f, %.3f, %.3f]", _msg->pose.position.x, _msg->pose.position.y, _msg->pose.position.z);
+          return; }
+        nearest_neighbors = fmin(nearest_neighbors, max_nn);
+        std::vector<float> score(nearest_neighbors), d_q(nearest_neighbors), 
+                           d_j(nearest_neighbors), d_xyz(nearest_neighbors);
+        Eigen::Quaterniond q_actual(end_effector_pose_.rotation());
+        Eigen::Quaterniond q;        
+        for (std::size_t i=0; i < nearest_neighbors; ++i)
+        {
+          // http://math.stackexchange.com/questions/90081/quaternion-distance
+          q.w() = metrics_db_["orientations"][i][0];
+          q.x() = metrics_db_["orientations"][i][1];
+          q.y() = metrics_db_["orientations"][i][2];
+          q.z() = metrics_db_["orientations"][i][3];
+          d_q[i] = 1 - pow(q.dot(q_actual), 2.0);
+          d_xyz[i] = sqrtf(dists[0][i]);
+          d_j[i] = 0;
+          for(std::size_t j=0; j < joint_names_.size(); ++j)
+          {
+            float j_error = fabs(current_joint_values[i] - metrics_db_["joint_states"][indices[0][i]][j]);
+            d_j[i] = fmax(d_j[i], j_error);
+          }
+          // Heuristics to improve the ik solution
+          score[i] = d_xyz[i] + d_j[i];
           //~ score[i] = 0.5*d_q[i] + 0.5*d_j[i];
-        //~ }
-        //~ std::size_t choice_idx = std::min_element(score.begin(), score.end()) - score.begin();
-        //~ ROS_DEBUG("nn [%d] choice [%d] score [%f] d_q [%f] d_xyz [%f] d_j [%f]", nearest_neighbors, 
-                    //~ (int)choice_idx, score[choice_idx], d_q[choice_idx], d_xyz[choice_idx], d_j[choice_idx]);
-        //~ // Populate the new joint_values
-        //~ std::ostringstream new_str;
-        //~ new_str << "new_joint_values : [";
-        //~ for(std::size_t i=0; i < joint_names_.size(); ++i)
-        //~ {
-          //~ new_joint_values.push_back(metrics_db_["joint_states"][indices[0][choice_idx]][i]);
-          //~ new_str << new_joint_values[i] << " ";            
-        //~ }
-        //~ new_str << "]";
-        //~ ik_kinematics_->setJointPositions(new_joint_values);
-        //~ // Debugging
-        //~ if (ros::Time::now() - last_motion_print_  >= ros::Duration(1.0))
-        //~ {
-          //~ last_motion_print_ = ros::Time::now();
-          //~ ROS_DEBUG_STREAM(current_str.str());
-          //~ ROS_DEBUG_STREAM("Index [" << choice_idx << "] Distance [" << dists[0][choice_idx] << "]");
-          //~ ROS_DEBUG_STREAM(new_str.str());
-        //~ }
+        }
+        std::size_t choice_idx = std::min_element(score.begin(), score.end()) - score.begin();
+        ROS_DEBUG("nn [%d] choice [%d] score [%f] d_q [%f] d_xyz [%f] d_j [%f]", nearest_neighbors, 
+                    (int)choice_idx, score[choice_idx], d_q[choice_idx], d_xyz[choice_idx], d_j[choice_idx]);
+        // Populate the new joint_values
+        std::ostringstream new_str;
+        new_str << "new_joint_values : [";
+        for(std::size_t i=0; i < joint_names_.size(); ++i)
+        {
+          new_joint_values.push_back(metrics_db_["joint_states"][indices[0][choice_idx]][i]);
+          new_str << new_joint_values[i] << " ";            
+        }
+        new_str << "]";
+        ik_kinematics_->setJointPositions(new_joint_values);
+        // Debugging
+        if (ros::Time::now() - last_motion_print_  >= ros::Duration(1.0))
+        {
+          last_motion_print_ = ros::Time::now();
+          ROS_DEBUG_STREAM(current_str.str());
+          ROS_DEBUG_STREAM("Index [" << choice_idx << "] Distance [" << dists[0][choice_idx] << "]");
+          ROS_DEBUG_STREAM(new_str.str());
+        }
       }
       // Command the robot to the new joint_values
       ROS_DEBUG("Joint names: %d, Joint values: %d", int(joint_names_.size()), int(new_joint_values.size()));
@@ -243,6 +287,20 @@ class CartesianController
         ROS_DEBUG("Joint %s: %f", joint_names_[i].c_str(), new_joint_values[i]);
       }
       control_publisher_.publish(cmd_msg);
+    }
+
+    std::string getFolderName(const std::string& folder_key)
+    {
+      std::ostringstream folder_name;
+      folder_name << getenv("HOME") << "/.openrave/robot." << folder_key << "/";
+      return folder_name.str();
+    }
+
+    std::string getFilename(const std::string& database, const std::string& folder_key, const std::string& file_key)
+    {
+      std::ostringstream filename;
+      filename << getFolderName(folder_key) << database << "." << file_key << ".pp";
+      return filename.str();
     }
 };
 
