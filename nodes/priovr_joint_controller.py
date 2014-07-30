e30c0ddbf0ac842fe58aaa1e0d80315e061d49e5
fsuarez6 <fsuarez6@gmail.com>
2014-07-30 18:49:00 -0400

    Added joint limits verification to the priovr joint controller node

--- nodes/priovr_joint_controller.py
+++ nodes/priovr_joint_controller.py
@@ -3,6 +3,8 @@ import rospy, serial, sys, math
 # Messages
 from sensor_msgs.msg import JointState
 from baxter_core_msgs.msg import JointCommand
+# URDF
+from urdf_parser_py.urdf import URDF
 
 
 BAXTER_JOINTS = { 'left_s0': 0,
@@ -21,8 +23,31 @@ BAXTER_JOINTS = { 'left_s0': 0,
                   'right_w2': 0
                 }
 
+
+class JointLimit(object):
+  def __init__(self):
+    self.lower = None
+    self.upper = None
+    self.effort = None
+    self.velocity = None
+  
+  def __str__(self):
+    msg = 'lower: [%s] upper: [%s] effort: [%s] velocity: [%s]' % (self.lower, self.upper, self.effort, self.velocity)
+    return msg
+
+
 class JointController(object):
   def __init__(self):
+    # Get the joint limits
+    self.joint_limits = dict()
+    robot_urdf = URDF.from_parameter_server()
+    for name, joint in robot_urdf.joint_map.items():
+      if name in BAXTER_JOINTS.keys():
+        self.joint_limits[name] = JointLimit()
+        self.joint_limits[name].lower = joint.limit.lower
+        self.joint_limits[name].upper = joint.limit.upper
+        self.joint_limits[name].effort = joint.limit.effort
+        self.joint_limits[name].velocity = joint.limit.velocity
     # Set-up publishers/subscribers
     rospy.Subscriber('joint_states', JointState, self.joint_states_cb)
     self.left_arm = rospy.Publisher('/robot/limb/left/joint_command', JointCommand)
@@ -41,6 +66,12 @@ class JointController(object):
       if joint_name not in BAXTER_JOINTS.keys():
         rospy.logwarn('Unknown joint: %s' % joint_name)
         continue
+      # Skip command if it's out of the joint limits
+      lower = self.joint_limits[joint_name].lower
+      upper = self.joint_limits[joint_name].upper
+      if not (lower <= msg.position[i] <= upper):
+        continue
+      # Append command to the corresponding arm
       if 'left_' in joint_name:
         left_msg.names.append(joint_name)
         left_msg.command.append(msg.position[i] + BAXTER_JOINTS[joint_name])
