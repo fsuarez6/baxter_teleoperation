eb53778e32c332ba8a568d14a48594e0b5fcbc67
fsuarez6 <fsuarez6@gmail.com>
2014-08-21 14:34:18 -0400

    PyKDL Cartesian Controller seems to be working properly

--- baxter_teleop/nodes/pykdl_cartesian_controller.py
+++ baxter_teleop/nodes/pykdl_cartesian_controller.py
@@ -5,7 +5,7 @@ import numpy as np
 # PyKDL
 import PyKDL
 from tf_conversions import posemath
-from pykdl_utils.kdl_kinematics import KDLKinematics, joint_kdl_to_list
+from pykdl_utils.kdl_kinematics import KDLKinematics, joint_kdl_to_list, joint_list_to_kdl, kdl_to_mat
 # URDF
 from urdf_parser_py.urdf import URDF
 # Baxter Python Interface
@@ -14,7 +14,24 @@ import baxter_interface
 from baxter_teleop.utils import read_parameter
 # Messages
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Empty, UInt16
 
+
+def baxter_to_kdl_frame(pose):
+  if pose.has_key('orientation') and pose.has_key('position'):
+    rot = PyKDL.Rotation.Quaternion(*pose['orientation'])
+    vec = PyKDL.Vector(*pose['position'])
+    return PyKDL.Frame(rot, vec)
+  else:
+    return None
+  
+def baxter_to_kdl_twist(twist):
+  if twist.has_key('linear') and twist.has_key('angular'):
+    vel = PyKDL.Vector(*twist['linear'])
+    rot = PyKDL.Vector(*twist['angular'])
+    return PyKDL.Twist(vel, rot)
+  else:
+    return None
 
 class CartesianController(object):
   def __init__(self, limb):
@@ -22,17 +39,15 @@ class CartesianController(object):
       rospy.logerr('Unknown limb name [%s]' % limb)
       return
     # Read the controllers parameters
-    self.kp = PyKDL.JntArray(6)
-    self.kd = PyKDL.JntArray(6)
-    for i in range(6):
-      self.kp[i] = 100
-      self.kd[i] = 1
-    self.publish_rate = read_parameter('~publish_rate', 100)
+    gains = read_parameter('~gains', dict())
+    self.kp = joint_list_to_kdl(gains['Kp'])
+    self.kd = joint_list_to_kdl(gains['Kd'])
+    self.publish_rate = read_parameter('~publish_rate', 500)
     self.frame_id = read_parameter('~frame_id', 'base')
     self.timeout = read_parameter('~timeout', 0.1)       # seconds
     # Set-up baxter interface
     self.arm_interface = baxter_interface.Limb(limb)
-    # set_joint_velocities must be commanded at a rate great than the timeout specified by set_command_timeout.
+    # set_joint_torques must be commanded at a rate great than the timeout specified by set_command_timeout.
     # If the timeout is exceeded before a new set_joint_velocities command is received, the controller will switch
     # modes back to position mode for safety purposes.
     self.arm_interface.set_command_timeout(self.timeout)
@@ -40,61 +55,83 @@ class CartesianController(object):
     self.urdf = URDF.from_parameter_server(key='robot_description')
     self.tip_link = '%s_gripper' % limb
     self.kinematics = KDLKinematics(self.urdf, self.frame_id, self.tip_link)
+    self.fk_vel_solver = PyKDL.ChainFkSolverVel_recursive(self.kinematics.chain)
+    # Adjust the publish rate of baxter's state
+    joint_state_pub_rate = rospy.Publisher('/robot/joint_state_publish_rate', UInt16)
     # Initialize the arm
     self.arm_interface.move_to_neutral(timeout=10.0)
     # Get the joint names
     self.joint_names = self.arm_interface.joint_names()
     self.num_joints = len(self.joint_names)
     # Set-up publishers/subscribers
+    self.suppress_grav_comp = rospy.Publisher('/robot/limb/%s/suppress_gravity_compensation' % limb, Empty)
+    joint_state_pub_rate.publish(int(self.publish_rate))
     rospy.Subscriber('/baxter/%s_ik_command' % limb, PoseStamped, self.ik_command_cb)
+    rospy.loginfo('Running Cartesian controller for the %s arm' % limb)
+    # Start torque controller timer
+    self.cart_command = None
+    self.torque_controller_timer = rospy.Timer(rospy.Duration(1.0/self.publish_rate), self.torque_controller_cb)
 
   def shutdown(self):
-    pass
-  
-  def get_kdl_angles(self):
-    q = PyKDL.JntArray(self.num_joints)
+    self.torque_controller_timer.shutdown()
+    self.arm_interface.exit_control_mode()
+
+  def get_kdl_array_vel(self):
+    out = PyKDL.JntArrayVel(self.num_joints)
     joint_angles = self.arm_interface.joint_angles()
-    for i, name in enumerate(self.joint_names):
-      q[i] = joint_angles[name]
-    return q
-  
-  def get_kdl_velocities(self):
-    qdot = PyKDL.JntArray(self.num_joints)
     joint_velocities = self.arm_interface.joint_velocities()
     for i, name in enumerate(self.joint_names):
-      qdot[i] = joint_velocities[name]
-    return qdot
+      out.q[i] = joint_angles[name]
+      out.qdot[i] = joint_velocities[name]
+    return out
   
   def jacobian_transpose(self, J):
-    J_transpose = PyKDL.Jacobian(J.columns())
-    for i in range(J.rows()):
-      for j in range(J.columns()):
-        J_transpose[j,i] = J[i,j]
-    return J_transpose
+    J_mat = kdl_to_mat(J)
+    return J_mat.T
   
   def ik_command_cb(self, msg):
+    self.cart_command = msg
+    # Add stamp because the console app don't use it
+    self.cart_command.header.stamp = rospy.Time.now()
+
+  def torque_controller_cb(self, event):
+    """
+    Implementation of a Cartesian controller with PyKDL
+    
+    """
+    if rospy.is_shutdown() or self.cart_command == None:
+      return
+    elapsed_time = rospy.Time.now() - self.cart_command.header.stamp
+    if elapsed_time.to_sec() > self.timeout:
+      return
     # TODO: Validate msg.header.frame_id
-    x_target = posemath.fromMsg(msg.pose)
+    # Create a copy of the message so it's safe thread
+    x_target = posemath.fromMsg(self.cart_command.pose)
     # Get positions and velocities
-    q = self.get_kdl_angles()
-    qdot = self.get_kdl_velocities()
-    # Compute the forward kinematics and Jacobian (at this location)
-    x = posemath.fromMatrix(self.kinematics.forward(joint_kdl_to_list(q)))
-    # Use the jacobian solver directly to get the PyKDL.Jacobian object
-    J = PyKDL.Jacobian(self.num_joints)
-    self.kinematics._jac_kdl.JntToJac(q, J)
-    # TODO: Function to multiply a KDL::Jacobian with a KDL::JntArray to get a KDL::Twist, it should not be used 
-    # to calculate the forward velocity kinematics, the solver classes are built for this purpose. J*q = t
-    # Implement ChainFkSolverVel_recursive
-    xdot = J * qdot
+    jnt_array_vel = self.get_kdl_array_vel()
+    # Get the current forward kinematics of the robot
+    x = baxter_to_kdl_frame(self.arm_interface.endpoint_pose())
+    xdot = baxter_to_kdl_twist(self.arm_interface.endpoint_velocity())
     # Calculate a Cartesian restoring wrench
-    x_error = PyKDL.diff(x, x_target)
-    wrench = PyKDL.Wrench(6)
-    for i in range(6):
-      wrench[i] = -(self.kp[i] * x_error[i] - self.kd[i] * xdot[i])
-    # Convert the force into a set of joint torques
+    x_error = PyKDL.diff(x_target, x)
+    wrench = np.matrix(np.zeros(6)).T
+    for i in range(len(wrench)):
+      wrench[i] = -(self.kp[i] * x_error[i] + self.kd[i] * xdot[i])
+    # Calculate the jacobian
+    J = PyKDL.Jacobian(self.num_joints)
+    self.kinematics._jac_kdl.JntToJac(jnt_array_vel.q, J)
+    # Convert the force into a set of joint torques. tau = J^T * wrench
     tau = self.jacobian_transpose(J) * wrench
-    print 'fuck!'
+    # TODO: Suppress gravity compensation??
+    #~ self.suppress_grav_comp.publish(Empty())
+    # Populate the joint_torques
+    joint_torques = dict()
+    for i, name in enumerate(self.joint_names):
+      joint_torques[name] = tau[i]
+    try:
+      self.arm_interface.set_joint_torques(joint_torques)
+    except rospy.ROSException:
+      pass
 
 
 if __name__ == '__main__':
