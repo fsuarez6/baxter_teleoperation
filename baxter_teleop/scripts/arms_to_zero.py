b6c35f2ae3ca78b135cbdb90d27230e6048dee0b
fsuarez6 <fsuarez6@gmail.com>
2014-08-21 16:00:39 -0400

    Removed wrong license and copyright

--- baxter_teleop/scripts/arms_to_zero.py
+++ baxter_teleop/scripts/arms_to_zero.py
@@ -1,26 +1,4 @@
 #!/usr/bin/env python
-
-"""
-    Control the joints of a robot using a skeleton tracker such as the
-    OpenNI tracker package in junction with a Kinect RGB-D camera.
-    
-    Based on the Pi Robot Project: http://www.pirobot.org
-    
-    Copyright (c) 2014 Francisco Suarez-Ruiz.  All rights reserved.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-    
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details at:
-    
-    http://www.gnu.org/licenses/gpl.html
-"""
-
 import rospy
 from math import pi
 from baxter_core_msgs.msg import JointCommand
